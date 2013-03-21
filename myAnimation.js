cdc4cb10306bf07e9d3f7a51f6da7195982f3318
mannyjl625 <mjl288@scarlemail.rutgers.edu>
2013-03-20 22:35:01 -0400

    updated repo

--- myAnimation.js
+++
@@ -1,9 +0,0 @@
-var canvas = = document.getElementById('screen');
-var ctx = canvas.getContexts('2d');
-
-var i = 0;
-setInterval(function(){
-	ctx.clearRect(0, 0, 240, 160); // vim delete line, undo, node
-	ctxfillRect(20+i, 20+i, 50, 50);
-	i++
-}, 1000);
