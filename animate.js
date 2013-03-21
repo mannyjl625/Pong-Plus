ae05cb213bb84cd456a9977be33bea95ba8c24ea
mannyjl625 <mjl288@scarlemail.rutgers.edu>
2013-03-20 22:32:54 -0400

    rectangles drawn

--- animate.js
+++ animate.js
@@ -1,11 +1,33 @@
+var canvasH = 340;
+var canvasW = 600;
+
+setTimeout(function(){
+    console.log("world");
+
+}, 3000);
+
+console.log("hello");
+
+// grabs canvas by ID from index.php
 var canvas = document.getElementById('screen');
+
+/* getContext makes context object that is filled with functions for drawing*/
 var ctx = canvas.getContext('2d');
 
+ //creates red object
 ctx.fillStyle = "rgb(200, 0, 0)";
 
-var xLocation = 0;
-setInterval(function(){
-	ctx.clearRect(0, 0, 240, 160);
-	xLocation++;
-	ctx.fillRect(xLocation, 20, 50, 50);
-}, 100);
+//sets object to coordinates 20,20 75px wide 50px hieght
+ctx.fillRect(20, 20, 75, 50);  
+
+//sets color with fourth argument for opacity
+ctx.fillStyle = "rgba(0, 0, 200, 0.5)" 
+
+ctx.fillRect(40, 40, 50, 50);
+
+setTimeout(function(){
+    ctx.clearRect(0, 0, canvasW, canvasH);
+    console.log("clear");
+}, 5000);
+
+
