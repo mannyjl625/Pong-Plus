cc894d0875f8b402b7d8591b6844d6a845311b60
mannyjl625 <mjl288@scarlemail.rutgers.edu>
2013-03-21 00:40:32 -0400

    got block to move

--- animate.js
+++ animate.js
@@ -11,23 +11,28 @@ console.log("hello");
 // grabs canvas by ID from index.php
 var canvas = document.getElementById('screen');
 
-/* getContext makes context object that is filled with functions for drawing*/
+//getContext makes context object that is filled with functions for drawing
 var ctx = canvas.getContext('2d');
-
+/*
  //creates red object
-ctx.fillStyle = "rgb(200, 0, 0)";
+ctx.fillStyle = "rgb(0, 0, 0)";
 
 //sets object to coordinates 20,20 75px wide 50px hieght
 ctx.fillRect(20, 20, 75, 50);  
-
 //sets color with fourth argument for opacity
 ctx.fillStyle = "rgba(0, 0, 200, 0.5)" 
 
 ctx.fillRect(40, 40, 50, 50);
+*/
 
-setTimeout(function(){
+var x = 30;
+var y = 30
+ctx.fillStyle = "rgb(0, 0, 0)";
+setInterval(function(){
+    x++;
+    y++;
     ctx.clearRect(0, 0, canvasW, canvasH);
-    console.log("clear");
-}, 5000);
+    ctx.fillRect(x, y, 75, 50);
+}, 25);
 
 
